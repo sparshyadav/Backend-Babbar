b088b597247ba24064f5830fbc1d6028c667a5a7
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-12-14 11:12:41 +0530

    Using body-parser

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use((bodyParser.json()));
 
 app.get("/", (req, res) => {
     res.send("Hello There");
 });
 
 app.post("api/cars", (req, res) => {
-    const { name, brand } = request.body;
+    const { name, brand } = req.body;
     console.log(name);
     console.log(brand);
     res.send("Car Submitted Successfully");
