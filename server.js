401f850711050c3c73d34ebdfe02821a75d7fbd5
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-12-14 10:48:05 +0530

    Creating POST Request

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-const express=require("express");
+const express = require("express");
 
-const app=express();
+const app = express();
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.send("Hello There");
 });
 
-app.listen(3000, ()=>console.log("Server Started"));
+app.post("api/cars", (req, res) => {
+    const { name, brand } = request.body;
+    console.log(name);
+    console.log(brand);
+    res.send("Car Submitted Successfully");
+});
+
+app.listen(3000, () => console.log("Server Started")); 
