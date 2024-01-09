3a0d1dbb1802ae4648ad01061165778316f1a9c9
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-01-09 12:09:47 +0530

    Todo Project Completed

--- Todo/config/database.js
+++ Todo/config/database.js
@@ -2,10 +2,8 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const dbConnect = () => {
-    mongoose.connect(process.env.DATABASE_URL, {
-        // useNewUrlParser: true,
-        // useUnifiedTopology: true
-    }).then(() => console.log("Db Connection is Successfull"))
+    mongoose.connect(process.env.DATABASE_URL, {})
+        .then(() => console.log("Db Connection is Successfull"))
         .catch((error) => {
             console.log("An Error Occured")
             console.error(error.message);
