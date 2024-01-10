56ab44b74cc49d6280d5d448912742e922e6ec51
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-01-10 16:53:23 +0530

    Server Created

--- Blog/server.js
+++ Blog/server.js
@@ -1,6 +1,19 @@
-const express=require("express");
-const blogRoutes=require("./routes/blog");
+const express = require("express");
+const blogRoutes = require("./routes/blog");
+const dbConnect = require("./config/database");
 require("dotenv").config;
-const PORT=process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-const app=express();
+const app = express();
+
+app.use(express.json());
+
+app.use("/api")
+
+app.get("/", (req, res) => {
+    res.send(`<h1>This is HomePage</h1>`);
+})
+
+app.listen(PORT, () => console.log("Server Started"));
+
+dbConnect();
