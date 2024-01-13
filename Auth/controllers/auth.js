9a31dc18692c513631fec7be6b0ee5b2f31eb26e
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-01-13 14:55:54 +0530

    Login Controller Created

--- Auth/controllers/auth.js
+++ Auth/controllers/auth.js
@@ -1,5 +1,7 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../model/user");
+require("dotenv").config();
 
 exports.signup = async (req, res) => {
     try {
@@ -42,6 +44,67 @@ exports.signup = async (req, res) => {
         return res.status(500).json({
             success: false,
             message: "User Cannot be Registered, Please Try Again Later"
+        });
+    }
+}
+
+exports.login = async (req, res) => {
+    try {
+        // Data Fetch
+        const { email, password } = req.body;
+
+        // Validation of email and password
+        if (!email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "Please Fill All the Details Carefully"
+            });
+        }
+
+        // Checking if the user is saved in the Database
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).json({
+                success: false,
+                message: "User is Not Registered"
+            });
+        }
+
+        const payload = {
+            email: user.email,
+            id: user._id,
+            role: user.role
+        }
+        // Verify Password and Generate a JWT Token
+        if (await bcrypt.compare(password, user.password)) {
+            let token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "2h" });
+            user.token = token;
+            user.password = undefined;
+            const options = {
+                expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+                httpOnly: true
+            }
+
+            res.cookie("token", token, options).status(200).json({
+                success: true,
+                token,
+                user,
+                message: "User Logged in Successfully"
+            })
+        }
+        else {
+            console.log(error);
+            return res.status(403).json({
+                success: false,
+                message: "Password Incorrect"
+            })
+        }
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Login Failure"
         });
     }
 }
--- Auth/routes/user.js
+++ Auth/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const { login, signup } = require("../controllers/auth");
+const { auth, isStudent, isAdmin } = require("../middlewares/auth");
 
 router.post("/login", login);
 router.post("/signup", signup);
