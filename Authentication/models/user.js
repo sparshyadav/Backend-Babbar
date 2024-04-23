d233255836caca5230549d9970addf33d6bbc4d2
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-04-23 10:27:58 +0530

    User Schema Created

---
+++ Authentication/models/user.js
@@ -0,0 +1,26 @@
+const { default: mongoose } = require("mongoose");
+
+exports.userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ["Admin", "Student", "Visitor"]
+    }
+})
+
+module.export = mongoose.model("User", this.userSchema);
