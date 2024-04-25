6e537caa53b9c3c0d1b4947f0cedb27a39404a8b
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-04-25 11:59:04 +0530

    Handler Created for Local File Upload

--- File Upload/routes/fileUpload.js
+++ File Upload/routes/fileUpload.js
@@ -1,3 +1,7 @@
-const express=require("express");
-const router=express.Router();
+const express = require("express");
+const { localFileUpload } = require("../controllers/fileUpload");
+const router = express.Router();
 
+app.post("/localupload", localFileUpload);
+
+module.exports = router;
